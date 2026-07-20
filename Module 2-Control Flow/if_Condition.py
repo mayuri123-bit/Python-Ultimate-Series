18e9d7aa67e33e5b929ed01cf460918f49c11ecb
mayuri123-bit <mayurinarwade120@gmail.com>
2026-07-20 23:59:51 +0530

    Control Flow File Added

---
+++ Module 2-Control Flow/if_Condition.py
@@ -0,0 +1,27 @@
+#...If Condition
+num = 10
+
+if num > 0:
+    print("Positive Number")
+
+#...If Else Statement
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+#.....If Elif Else
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+elif marks >= 35:
+    print("Grade D")
+else:
+    print("Fail")
